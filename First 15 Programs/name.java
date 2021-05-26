33462591aa7bec66a643743bcc4adc0a2197de52
Murshid TP <murshidtp68@gmail.com>
2021-05-26 11:39:18 +0530

    4 prgms completed

---
+++ First 15 Programs/name.java
@@ -0,0 +1,6 @@
+public class name{
+    public static void main(String[] a){
+        System.out.println("Hello");
+        System.out.println("Murshid");
+    }
+}
---
+++ First 15 Programs/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] a) {
+        int num1=10;
+        int num2=20;
+        int sum;
+        sum=num1+num2;
+        System.out.println("sum of 2 numbers:"+sum);
+    }
+}
