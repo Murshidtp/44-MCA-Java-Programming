1568a89591dc888fe2d571e333e936fc707dfd3c
Murshid TP <murshidtp68@gmail.com>
2021-06-08 20:29:33 +0530

    Program modified

--- CO-1/Program1.java
+++ CO-1/Program1.java
@@ -1,32 +1,37 @@
 class product{
     String pcode;
     String pname;
-    int price;
+    double price;
+
+    String lowestPrice(product p1, product p2, product p3)
+    {
+        if(p1.price<p2.price && p1.price<p3.price){
+            return p1.pname;
+        }
+        else if(p2.price<p1.price && p2.price<p3.price){
+            return p2.pname;
+        }
+        else{
+            return p3.pname;
+        }
+    }
+    void SetValues(String pcode,String pname,double price){
+            this.pcode=pcode;
+            this.pname=pname;
+            this.price=price;
+    }
 }
 public class Program1 {
     public static void main(String[] args) {
         product p1=new product();
         product p2=new product();
         product p3=new product();
-        p1.pcode="p11";
-        p1.pname="Pen";
-        p1.price=10;
-
-        p2.pcode="p12";
-        p2.pname="Book";
-        p2.price=20;
-
-        p3.pcode="p13";
-        p3.pname="Pencil";
-        p3.price=5;
-        if(p1.price<p2.price && p1.price<p3.price){
-            System.out.println(p1.pname+" have less price");
-        }
-        else if(p2.price<p1.price && p2.price<p3.price){
-            System.out.println(p2.pname+" have less price");
-        }
-        else{
-            System.out.println(p3.pname+" have lowest price");
-        }
+        p1.SetValues("p11", "Redmi", 20000);
+        p2.SetValues("p12", "Iphone", 50000);
+        p3.SetValues("p13", "Samsung", 25000);
+        String lowest;
+        lowest=p1.lowestPrice(p2,p3,p1);
+        System.out.println(lowest+" has lowest price");
+        
     }
 }
