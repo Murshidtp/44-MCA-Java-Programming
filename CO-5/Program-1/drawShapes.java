007f4f741cd0f759aae09d873272b1780d236004
Murshid TP <murshidtp68@gmail.com>
2021-09-27 17:23:42 +0530

    output added

--- CO-5/Program-1/drawShapes.java
+++ CO-5/Program-1/drawShapes.java
@@ -1,14 +1,15 @@
-   
+/* Program to draw Circle, Rectangle, Line in Applet.  */
+
 import java.awt.*;
 import java.applet.*;
 /*
-<applet code="drawShapes" width=800 height=800>
+<applet code="Shapes" width=400 height=400>
 </applet>
 */
-public class drawShapes extends Applet {
+public class Shapes extends Applet {
   public void paint(Graphics g) {
     g.drawLine(10, 10, 100, 100);
-    g.drawRect(200, 250, 200, 350);
-    g.drawOval(500, 500, 300, 300);
+    g.drawRect(100, 150, 100, 150);
+    g.drawOval(200, 200, 200, 200);
   }
 }
